fcf84d20f7cc4e20a0b3b9a84bc6a99a4f91e21b
Mass-Github <massaoudhamani10@gmail.com>
2022-06-02 16:47:07 +0100

    first commit

--- connect.php
+++ connect.php
@@ -23,26 +23,5 @@ if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submit'])){
 		}
 	}
 } 
-/*
-// Connexion à la base de données
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test1', 'root', '');
-}
-	catch(Exception $e)
-{
-	die('Erreur : '.$e->getMessage());
-}
-// Insertion du message à l'aide d'une requête préparée
-	$req = $bdd->prepare('INSERT INTO users (name, email, phone, bgroup)
-	 VALUES(?, ?, ?, ?)');
-	$req->execute(array($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['bgroup']));
-	if($req){
-		echo 'Insertion reussie !';
-	}else{
-		echo 'Insertion non reussie !';
-	}
 
-// Redirection du visiteur vers la page du index
-	header('Location: index.php');*/
 ?>
